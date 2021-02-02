9cb2ab66a22bbba27cb3a2176d6d59c55b0989ad
DestroyedClone <DestroyedClone@gmail.com>
2021-02-02 08:29:48 -0800

    Add project files.

---
+++ AncientScepter/AncientScepterMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using MonoMod.Cil;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using BepInEx.Configuration;
+using Mono.Cecil.Cil;
+using System;
+using TMPro;
+using UnityEngine.Networking;
+using Path = System.IO.Path;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+namespace AncientScepter
+{
+    public class AncientScepterMain
+    {
+    }
+}
