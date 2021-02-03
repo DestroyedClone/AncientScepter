1ca2ca638405ea03711e94cf1a778a517fb33bcc
DestroyedClone <DestroyedClone@gmail.com>
2021-02-02 16:49:06 -0800

    s

--- AncientScepter/AncientScepterItem.cs
+++ AncientScepter/AncientScepterItem.cs
@@ -74,6 +74,8 @@ namespace AncientScepter
             "And before you ask, yes, the handle is designed to be hard to hold, culls the weak.");
         public override ItemTier Tier => ItemTier.Tier3;
         public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.Utility };
+        public override string ItemModelPath => "@Aetherium:Assets/Models/Prefabs/Item/ShieldingCore/ShieldingCore.prefab";
+        public override string ItemIconPath => "@Aetherium:Assets/Textures/Icons/Item/ShieldingCoreIconAlt.png";
 
         public override bool AIBlacklisted => true;
         //public override string ItemModelPath => "@Aetherium:Assets/Models/Prefabs/Item/ShieldingCore/ShieldingCore.prefab";
@@ -118,6 +120,9 @@ namespace AncientScepter
             };
         }
 
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        { return null; }
+
         internal List<ScepterSkill> skills = new List<ScepterSkill>();
 
         public AncientScepterItem()
@@ -156,6 +161,196 @@ namespace AncientScepter
             GetStatCoefficients += ShieldedCoreArmorCalc;
         }
 
+        public void SetupAttributes()
+        {
+            base.SetupAttributes();
 
+            foreach (var skill in skills)
+            {
+                skill.SetupAttributes();
+                RegisterScepterSkill(skill.myDef, skill.targetBody, skill.targetSlot, skill.targetVariantIndex);
+            }
+        }
+
+        public void Install()
+        {
+            On.RoR2.CharacterBody.OnInventoryChanged += On_CBOnInventoryChanged;
+            On.RoR2.CharacterMaster.GetDeployableSameSlotLimit += On_CMGetDeployableSameSlotLimit;
+            On.RoR2.GenericSkill.SetSkillOverride += On_GSSetSkillOverride;
+
+            foreach (var skill in skills)
+            {
+                skill.LoadBehavior();
+            }
+
+            foreach (var cm in AliveList())
+            {
+                if (!cm.hasBody) continue;
+                var body = cm.GetBody();
+                HandleScepterSkill(body);
+            }
+        }
+
+        public void Uninstall()
+        {
+            On.RoR2.CharacterBody.OnInventoryChanged -= On_CBOnInventoryChanged;
+            On.RoR2.CharacterMaster.GetDeployableSameSlotLimit -= On_CMGetDeployableSameSlotLimit;
+            On.RoR2.GenericSkill.SetSkillOverride -= On_GSSetSkillOverride;
+
+            foreach (var cm in AliveList())
+            {
+                if (!cm.hasBody) continue;
+                var body = cm.GetBody();
+                HandleScepterSkill(body, true);
+            }
+
+            foreach (var skill in skills)
+            {
+                skill.UnloadBehavior();
+            }
+        }
+
+        public void InstallLanguage()
+        {
+            foreach (var skill in skills)
+            {
+                if (skill.oldDescToken == null)
+                {
+                    ClassicItemsPlugin._logger.LogError(skill.GetType().Name + " oldDescToken is null!");
+                    continue;
+                }
+                languageOverlays.Add(LanguageAPI.AddOverlay(skill.newDescToken, Language.GetString(skill.oldDescToken) + skill.overrideStr, Language.currentLanguageName));
+            }
+        }
+
+        bool handlingOverride = false;
+        private void On_GSSetSkillOverride(On.RoR2.GenericSkill.orig_SetSkillOverride orig, GenericSkill self, object source, SkillDef skillDef, GenericSkill.SkillOverridePriority priority)
+        {
+            if (stridesInteractionMode != StridesInteractionMode.ScepterTakesPrecedence
+                || skillDef.skillIndex != CharacterBody.CommonAssets.lunarUtilityReplacementSkillDef.skillIndex
+                || !(source is CharacterBody body)
+                || body.inventory.GetItemCount(catalogIndex) < 1
+                || handlingOverride)
+                orig(self, source, skillDef, priority);
+            else
+            {
+                handlingOverride = true;
+                HandleScepterSkill(body);
+                handlingOverride = false;
+            }
+        }
+
+        private int On_CMGetDeployableSameSlotLimit(On.RoR2.CharacterMaster.orig_GetDeployableSameSlotLimit orig, CharacterMaster self, DeployableSlot slot)
+        {
+            var retv = orig(self, slot);
+            if (slot != DeployableSlot.EngiTurret) return retv;
+            var sp = self.GetBody()?.skillLocator?.special;
+            if (!sp) return retv;
+            if (sp.skillDef == skills.First(x => x is EngiTurret2).myDef)
+                return retv + 1;
+            if (sp.skillDef == skills.First(x => x is EngiWalker2).myDef)
+                return retv + 2;
+            return retv;
+        }
+
+        private class ScepterReplacer
+        {
+            public string bodyName;
+            public SkillSlot slotIndex;
+            public int variantIndex;
+            public SkillDef replDef;
+        }
+
+        private readonly List<ScepterReplacer> scepterReplacers = new List<ScepterReplacer>();
+        private readonly Dictionary<string, SkillSlot> scepterSlots = new Dictionary<string, SkillSlot>();
+
+        public bool RegisterScepterSkill(SkillDef replacingDef, string targetBodyName, SkillSlot targetSlot, int targetVariant)
+        {
+            if (targetVariant < 0)
+            {
+                ClassicItemsPlugin._logger.LogError("Can't register a scepter skill to negative variant index");
+                return false;
+            }
+            if (scepterReplacers.Exists(x => x.bodyName == targetBodyName && (x.slotIndex != targetSlot || x.variantIndex == targetVariant)))
+            {
+                ClassicItemsPlugin._logger.LogError("A scepter skill already exists for this character; can't add multiple for different slots nor for the same variant");
+                return false;
+            }
+            scepterReplacers.Add(new ScepterReplacer { bodyName = targetBodyName, slotIndex = targetSlot, variantIndex = targetVariant, replDef = replacingDef });
+            scepterSlots[targetBodyName] = targetSlot;
+            return true;
+        }
+
+        bool handlingInventory = false;
+        private void On_CBOnInventoryChanged(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self)
+        {
+            orig(self);
+            if (handlingInventory) return;
+            handlingInventory = true;
+            if (!HandleScepterSkill(self))
+            {
+                if (GetCount(self) > 0)
+                {
+                    Reroll(self, GetCount(self));
+                }
+            }
+            else if (GetCount(self) > 1 && rerollExtras)
+            {
+                Reroll(self, GetCount(self) - 1);
+            }
+            handlingInventory = false;
+        }
+
+        private void Reroll(CharacterBody self, int count)
+        {
+            if (count <= 0) return;
+            var list = Run.instance.availableTier3DropList.Except(new[] { pickupIndex }).ToList();
+            for (var i = 0; i < count; i++)
+            {
+                self.inventory.RemoveItem(catalogIndex, 1);
+                self.inventory.GiveItem(PickupCatalog.GetPickupDef(list[UnityEngine.Random.Range(0, list.Count)]).itemIndex);
+            }
+        }
+
+        private bool HandleScepterSkill(CharacterBody self, bool forceOff = false)
+        {
+            bool hasStrides = self.inventory.GetItemCount(ItemIndex.LunarUtilityReplacement) > 0;
+            if (self.skillLocator && self.master?.loadout != null)
+            {
+                var bodyName = BodyCatalog.GetBodyName(self.bodyIndex);
+
+                var repl = scepterReplacers.FindAll(x => x.bodyName == bodyName);
+                if (repl.Count > 0)
+                {
+                    SkillSlot targetSlot = scepterSlots[bodyName];
+                    if (targetSlot == SkillSlot.Utility && stridesInteractionMode == StridesInteractionMode.ScepterRerolls && hasStrides) return false;
+                    var targetSkill = self.skillLocator.GetSkill(targetSlot);
+                    if (!targetSkill) return false;
+                    var targetSlotIndex = self.skillLocator.GetSkillSlotIndex(targetSkill);
+                    var targetVariant = self.master.loadout.bodyLoadoutManager.GetSkillVariant(self.bodyIndex, targetSlotIndex);
+                    var replVar = repl.Find(x => x.variantIndex == targetVariant);
+                    if (replVar == null) return false;
+                    if (!forceOff && GetCount(self) > 0)
+                    {
+                        if (stridesInteractionMode == StridesInteractionMode.ScepterTakesPrecedence && hasStrides)
+                        {
+                            self.skillLocator.utility.UnsetSkillOverride(self, CharacterBody.CommonAssets.lunarUtilityReplacementSkillDef, GenericSkill.SkillOverridePriority.Replacement);
+                        }
+                        targetSkill.SetSkillOverride(self, replVar.replDef, GenericSkill.SkillOverridePriority.Upgrade);
+                    }
+                    else
+                    {
+                        targetSkill.UnsetSkillOverride(self, replVar.replDef, GenericSkill.SkillOverridePriority.Upgrade);
+                        if (stridesInteractionMode == StridesInteractionMode.ScepterTakesPrecedence && hasStrides)
+                        {
+                            self.skillLocator.utility.SetSkillOverride(self, CharacterBody.CommonAssets.lunarUtilityReplacementSkillDef, GenericSkill.SkillOverridePriority.Replacement);
+                        }
+                    }
+
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
