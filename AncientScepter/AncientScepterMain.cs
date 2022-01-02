6fb917341f4ecd582c0f07c80d3ea9a7031021b2
DestroyedClone <destroyedclone@gmail.com>
2022-01-01 22:46:23 -0800

    codemaid pass + version update from 1.0.7 to 1.0.8

--- AncientScepter/AncientScepterMain.cs
+++ AncientScepter/AncientScepterMain.cs
@@ -10,8 +10,6 @@ using System.Security;
 using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.Networking;
-using RoR2.Artifacts;
-using System.Runtime.CompilerServices;
 
 [module: UnverifiableCode]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -25,7 +23,7 @@ namespace AncientScepter
     [BepInDependency(TILER2.TILER2Plugin.ModGuid, BepInDependency.DependencyFlags.SoftDependency)]
     public class AncientScepterMain : BaseUnityPlugin
     {
-        public const string ModVer = "1.0.7";
+        public const string ModVer = "1.0.8";
         public const string ModName = "StandaloneAncientScepter";
         public const string ModGuid = "com.DestroyedClone.AncientScepter";
 
