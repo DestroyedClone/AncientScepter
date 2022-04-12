8014661f958e645a968aca11d9d8aa68e5b8fcb3
DestroyedClone <destroyedclone@gmail.com>
2022-04-12 03:34:24 -0700

    Added CloneSkillDef overfills(?) RailgunnerSkillDef and VoidSurvivorSkillDef

--- AncientScepter/SkillUtil.cs
+++ AncientScepter/SkillUtil.cs
@@ -251,10 +251,6 @@ namespace AncientScepter
         /// <returns>A clone of oldDef, shallow changes to which will not affect the original.</returns>
         public static SkillDef CloneSkillDef(SkillDef oldDef)
         {
-            if (!oldDef)
-            {
-                Debug.LogError($"CloneSkillDef: {oldDef} does not exist!");
-            }
             var newDef = ScriptableObject.CreateInstance<SkillDef>();
 
             newDef.skillName = oldDef.skillName;
@@ -291,9 +287,96 @@ namespace AncientScepter
             return newDef;
         }
 
-        public static RailgunSkillDef CloneRailgunSkillDef(RailgunSkillDef railgunSkillDef)
+        /// <summary>
+        /// Clones an existing RailgunSkillDef into a new instance with the same values.
+        /// </summary>
+        /// <param name="oldDef">The RailgunSkillDef instance to clone.</param>
+        /// <returns>A clone of oldDef, shallow changes to which will not affect the original.</returns>
+        public static RailgunSkillDef CloneSkillDef(RailgunSkillDef oldDef)
         {
-            return null;
+            var newDef = ScriptableObject.CreateInstance<RailgunSkillDef>();
+
+            newDef.skillName = oldDef.skillName;
+            newDef.skillNameToken = oldDef.skillNameToken;
+            newDef.skillDescriptionToken = oldDef.skillDescriptionToken;
+            newDef.icon = oldDef.icon;
+
+            newDef.activationStateMachineName = oldDef.activationStateMachineName;
+            newDef.activationState = oldDef.activationState;
+
+            newDef.interruptPriority = oldDef.interruptPriority;
+
+            newDef.baseRechargeInterval = oldDef.baseRechargeInterval;
+            newDef.baseMaxStock = oldDef.baseMaxStock;
+            newDef.rechargeStock = oldDef.rechargeStock;
+            newDef.requiredStock = oldDef.requiredStock;
+            newDef.stockToConsume = oldDef.stockToConsume;
+            newDef.beginSkillCooldownOnSkillEnd = oldDef.beginSkillCooldownOnSkillEnd;
+            newDef.fullRestockOnAssign = oldDef.fullRestockOnAssign;
+            newDef.dontAllowPastMaxStocks = oldDef.dontAllowPastMaxStocks;
+
+            newDef.resetCooldownTimerOnUse = oldDef.resetCooldownTimerOnUse;
+
+            newDef.isCombatSkill = oldDef.isCombatSkill;
+
+            newDef.cancelSprintingOnActivation = oldDef.cancelSprintingOnActivation;
+            newDef.canceledFromSprinting = oldDef.canceledFromSprinting;
+            newDef.forceSprintDuringState = oldDef.forceSprintDuringState;
+
+            newDef.mustKeyPress = oldDef.mustKeyPress;
+
+            newDef.keywordTokens = oldDef.keywordTokens;
+
+            newDef.restockOnReload = oldDef.restockOnReload;
+            newDef.offlineIcon = oldDef.offlineIcon;
+
+            return newDef;
+        }
+
+
+        /// <summary>
+        /// Clones an existing VoidSurvivorSkillDef into a new instance with the same values.
+        /// </summary>
+        /// <param name="oldDef">The SkillDef instance to clone.</param>
+        /// <returns>A clone of oldDef, shallow changes to which will not affect the original.</returns>
+        public static VoidSurvivorSkillDef CloneSkillDef(VoidSurvivorSkillDef oldDef)
+        {
+            var newDef = ScriptableObject.CreateInstance<VoidSurvivorSkillDef>();
+
+            newDef.skillName = oldDef.skillName;
+            newDef.skillNameToken = oldDef.skillNameToken;
+            newDef.skillDescriptionToken = oldDef.skillDescriptionToken;
+            newDef.icon = oldDef.icon;
+
+            newDef.activationStateMachineName = oldDef.activationStateMachineName;
+            newDef.activationState = oldDef.activationState;
+
+            newDef.interruptPriority = oldDef.interruptPriority;
+
+            newDef.baseRechargeInterval = oldDef.baseRechargeInterval;
+            newDef.baseMaxStock = oldDef.baseMaxStock;
+            newDef.rechargeStock = oldDef.rechargeStock;
+            newDef.requiredStock = oldDef.requiredStock;
+            newDef.stockToConsume = oldDef.stockToConsume;
+            newDef.beginSkillCooldownOnSkillEnd = oldDef.beginSkillCooldownOnSkillEnd;
+            newDef.fullRestockOnAssign = oldDef.fullRestockOnAssign;
+            newDef.dontAllowPastMaxStocks = oldDef.dontAllowPastMaxStocks;
+
+            newDef.resetCooldownTimerOnUse = oldDef.resetCooldownTimerOnUse;
+
+            newDef.isCombatSkill = oldDef.isCombatSkill;
+
+            newDef.cancelSprintingOnActivation = oldDef.cancelSprintingOnActivation;
+            newDef.canceledFromSprinting = oldDef.canceledFromSprinting;
+            newDef.forceSprintDuringState = oldDef.forceSprintDuringState;
+
+            newDef.mustKeyPress = oldDef.mustKeyPress;
+
+            newDef.keywordTokens = oldDef.keywordTokens;
+
+            newDef.minimumCorruption = oldDef.minimumCorruption;
+            newDef.maximumCorruption = oldDef.maximumCorruption;
+            return newDef;
         }
     }
 }
